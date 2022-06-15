c82f10cb9f655d54d459570573d4854b27593790
Thays Omine <thaysomine@gmail.com>
2022-06-15 04:48:12 -0300

    feat: search user by name

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,6 +6,19 @@ export async function getUser (req, res) {
     try {
         const checkUser = await db.query(`SELECT * FROM users WHERE id = $1`, [id]);
         console.log(checkUser.rows);
+        res.status(200).send(checkUser.rows[0]);
+    } catch (e) {
+        console.log(`erro ao buscar usuario: ${e}`);
+        res.sendStatus(500);
+    }
+}
+
+export async function searchUser (req, res) {
+    const { user } = req.params;
+    
+    try {
+        const checkUser = await db.query(`SELECT * FROM users WHERE username ILIKE $1`, [`%${user}%`]);
+        console.log(checkUser.rows);
         res.status(200).send(checkUser.rows);
     } catch (e) {
         console.log(`erro ao buscar usuario: ${e}`);
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 // Router para usuário
 import {Router} from 'express';
 
-import { getUser } from '../controllers/userController.js';
+import { getUser, searchUser } from '../controllers/userController.js';
 import { validToken } from '../middlewares/token.js';
 
 const  userRouter = Router();
 
 userRouter.get('/user/:id', validToken, getUser);
+userRouter.get('/user/search/:user', validToken, searchUser);
 
 export default userRouter;

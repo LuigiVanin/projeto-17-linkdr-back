2e89a90586db08cd5808c526db94001cc5aa9e4c
Luis Felipe Vanin <luisfvaninmartins@gmail.com>
2022-06-17 01:36:17 -0300

    rota de tendings e middleware de autenticação provisório

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,31 @@
+import { tokenSchema } from "../schemas/authSchema.js";
+import userRepository from "../repositories/userRepository.js";
+
+const authentication = async (req, res, next) => {
+    let { authorization: token } = req.headers;
+
+    const validation = tokenSchema.validate(token);
+    if (validation.error) {
+        return res
+            .status(401)
+            .send(validation.error.details.map((i) => i.message));
+    }
+    token = token.replace("Bearer", "").trim();
+    try {
+        const user = await userRepository.getUserBySession(token);
+        if (!user.rowCount) {
+            return res.status(401).send({ message: "Token Inválido" });
+        }
+        res.locals.user = user.rows[0];
+        console.log("AUTENTICADO!");
+        next();
+    } catch (err) {
+        console.log(err);
+        if (err.name === "error" && err.table) {
+            return res.status(422).send({ details: err.detail });
+        }
+        return res.send(500);
+    }
+};
+
+export default authentication;
--- src/repositories/hashtagRepository.js
+++ src/repositories/hashtagRepository.js
@@ -3,17 +3,30 @@ import db from "../database.js";
 const getHashtagsByName = async (hashtagName) => {
     return db.query(
         `
-        SELECT count(l.id) as "likesCount", p.description, p.link, p.id as "postId", u.username, u.email, u."imageUrl"
+        SELECT count(l.id) as "likesCount", p.description, p.link, p.id as "postId", u.username, u.email, u."imageUrl", u.id as "userId"
         FROM posts p
         JOIN users u ON p."userId" = u.id
-        JOIN likes l ON p.id = l."postId"
         JOIN "postsHashtags" ph ON ph."postId" = p.id
         JOIN hashtags h ON h.id = ph."hashtagId" 
+        LEFT JOIN likes l ON p.id = l."postId"
         WHERE h.name = $1
-        GROUP BY p.id, p.description, p.link, u.username, u.email, u."imageUrl"
+        GROUP BY p.id, p.description, p.link, u.username, u.email, u."imageUrl", u.id
     `,
         [`#${hashtagName}`]
     );
 };
 
-export { getHashtagsByName };
+const getTrendingHashtags = async () => {
+    return db.query(
+        `
+        SELECT h.name, count(p.id) as "frequency" FROM hashtags h
+        JOIN "postsHashtags" ph ON ph."hashtagId" = h.id
+        JOIN posts p on p.id = ph."postId" 
+        GROUP BY h.name
+        ORDER BY "frequency" DESC
+        LIMIT 10
+        `
+    );
+};
+
+export { getHashtagsByName, getTrendingHashtags };

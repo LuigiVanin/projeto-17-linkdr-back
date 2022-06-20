45a1a4ec51156e840cb204c2522156f3926f7d59
thiagosouf <thiagosouf@gmail.com>
2022-06-20 10:39:23 -0300

    alinhando pull

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Aberta na porta ${PORT}`);

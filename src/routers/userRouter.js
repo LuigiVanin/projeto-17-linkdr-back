c2b549a0e18674cdb26ebb3d87e9073e6dd3884a
Luis Felipe Vanin <luisfvaninmartins@gmail.com>
2022-06-20 20:10:21 -0300

    middleware universal

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,13 @@
 // Router para usuário
-import {Router} from 'express';
+import { Router } from "express";
 
-import { getUser, searchUser } from '../controllers/userController.js';
-import { validToken } from '../middlewares/token.js';
+import { getUser, searchUser } from "../controllers/userController.js";
+import { validToken } from "../middlewares/token.js";
+import authentication from "../middlewares/authentication.js";
 
-const  userRouter = Router();
+const userRouter = Router();
 
-userRouter.get('/user/:id', validToken, getUser);
-userRouter.get('/search/:user', validToken, searchUser);
+userRouter.get("/user/:id", authentication, getUser);
+userRouter.get("/search/:user", authentication, searchUser);
 
 export default userRouter;

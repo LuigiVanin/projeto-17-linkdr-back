caf528a95219d5951a57a477e8fb18497de6c8e1
Augusto <acfb.app@gmail.com>
2022-06-21 15:32:07 -0300

    abrindo ultimo commit feito

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const configDatabase = {
+/*const configDatabase = {
   connectionString: process.env.DATABASE_URL
 };
 
@@ -13,9 +13,9 @@ if(process.env.MODE === "PROD") {
     rejectUnauthorized: false
   }
 }
-const db = new Pool(configDatabase);
+const db = new Pool(configDatabase);*/
 
-/*
+
 // conexao db para teste local:-----------------
 const db = new Pool({
   host: 'localhost',
@@ -25,6 +25,6 @@ const db = new Pool({
   database: 'linkr'
 });
 // ---------------------------------------------
-*/
+
 
 export default db;

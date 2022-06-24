456ef43770c273108926f25e82eb7206975ee39e
= <f7lipe@me.com>
2022-06-24 17:30:28 -0300

    fix: database connection

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const { Pool } = pg;
 
-/*const configDatabase = {
+const configDatabase = {
   connectionString: process.env.DATABASE_URL
 };
 
@@ -13,9 +13,9 @@ if(process.env.MODE === "PROD") {
     rejectUnauthorized: false
   }
 }
-const db = new Pool(configDatabase);*/
+const db = new Pool(configDatabase);
 
-// conexao db para teste local:-----------------
+/* conexao db para teste local:-----------------
 const db = new Pool({
   host: process.env.HOST,
   port: process.env.SQL_PORT,
@@ -23,6 +23,6 @@ const db = new Pool({
   password: process.env.PASSWORD,
   database: process.env.DATABASE
 });
-// ---------------------------------------------
+/---------------------------------------------*/ 
 
 export default db;
